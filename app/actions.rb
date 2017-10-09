2e2819750536ef564a5a3869c0543f22b5578a6b
Brian <brian.hall@adessopm.com>
2017-10-09 20:56:39 +0000

    sessions

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
@@ -7,6 +13,33 @@ get '/signup' do        # if a user navigates to the path "/signup",
     @user = User.new    # setup empty @usr object
     erb(:signup)        # render "app/views/signup.erb"
 end
+
+get '/login' do        # when a GET request comes into /login
+    erb(:login)        # render "app/views/login.erb"
+end
+
+post '/login' do        # when we submit a form with an action of /login
+    username = params[:username]
+    password = params[:password]
+    
+    # 1. find user by username
+    user = User.find_by(username: username)
+    
+        if user && user.password == password
+            session[:user_id] = user.id
+            redirect to('/')
+            
+        else
+            @error_message = "Login failed."
+            erb(:login)
+        end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
 
 post '/signup' do
     
@@ -19,7 +52,7 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
     
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
         
     else
         erb(:signup)
